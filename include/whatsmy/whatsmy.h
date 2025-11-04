7d98ff003de1f8f3e7d9f9648b76bcdc9cd389ca
enXov <125979840+enXov@users.noreply.github.com>
2025-11-04 16:13:25 +0300

    Bump version to 1.2.1

--- include/whatsmy/whatsmy.h
+++ include/whatsmy/whatsmy.h
@@ -13,7 +13,7 @@ namespace whatsmy {
 /**
  * Application version information
  */
-constexpr const char* VERSION = "1.2.0";
+constexpr const char* VERSION = "1.2.1";
 constexpr const char* APP_NAME = "whatsmy";
 
 /**
